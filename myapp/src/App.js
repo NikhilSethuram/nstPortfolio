eb9babe313107b820dadb5fb320c4a3739176bc4
NikhilSethuram <121523766+NikhilSethuram@users.noreply.github.com>
2023-11-21 23:51:35 -0600

    updated tech stack

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,6 +4,24 @@ import NavbarComponent from "./components/Navbar";
 import profilePic from "./images/profile-pic.jpeg";
 import { TypeAnimation } from "react-type-animation";
 import { GeistProvider, CssBaseline, Page } from "@geist-ui/react";
+import { SiSpringboot, SiScikitlearn, SiFlask } from "react-icons/si";
+
+import {
+  FaReact,
+  FaJava,
+  FaPython,
+  FaAngular,
+  FaNodeJs,
+  FaDocker,
+} from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import {
+  DiMongodb,
+  DiCss3,
+  DiMysql,
+  DiPostgresql,
+  DiSqllite,
+} from "react-icons/di";
 
 const introText = `
   Welcome to my website!
@@ -44,6 +62,23 @@ function App() {
           <div className="TechStackText">
             <h1>Tech Stack</h1>
           </div>
+        </div>
+        <div className="icons-container">
+          <FaJava size={100} />
+          <FaPython size={100} />
+          <IoLogoJavascript size={100} />
+          <FaReact size={100} />
+          <FaAngular size={100} />
+          <FaNodeJs size={100} />
+          <DiMongodb size={100} />
+          <DiCss3 size={100} />
+          <SiSpringboot size={100} />
+          <SiFlask size={100} />
+          <SiScikitlearn size={100} />
+          <DiMysql size={100} />
+          <DiPostgresql size={100} />
+          <DiSqllite size={100} />
+          <FaDocker size={100} />
         </div>
       </>
     </GeistProvider>
