36503666dc216d4c3f469c3f838014dadb13afa4
NikhilSethuram <121523766+NikhilSethuram@users.noreply.github.com>
2023-11-21 23:04:02 -0600

    intro+profilepic

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,52 @@
+import React from "react";
 import "./App.css";
+import NavbarComponent from "./components/Navbar";
+import profilePic from "./images/profile-pic.jpeg";
+import { TypeAnimation } from "react-type-animation";
+import { GeistProvider, CssBaseline, Page } from "@geist-ui/react";
+
+const introText = `
+  Welcome to my website!
+  I'm Nikhil Sethuram,
+  a sophomore at the University of Wisconsin-Madison.
+  I'm a full-stack developer.
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GeistProvider>
+      <CssBaseline />
+      <>
+        <div className="Navbar">{<NavbarComponent />}</div>
+        <div className="App">
+          <div className="Intro-container">
+            <Page>
+              <Page.Content>
+                <h1>Hi!</h1>
+                <h4 className="Intro">
+                  <TypeAnimation
+                    sequence={[introText, 5000, ""]}
+                    speed={50}
+                    style={{ whiteSpace: "pre-line", fontSize: "2em" }}
+                    repeat={Infinity}
+                  />
+                </h4>
+              </Page.Content>
+            </Page>
+          </div>
+          <div className="ProfilePicContainer">
+            <img src={profilePic} alt="Profile Pic" className="ProfilePic" />
+          </div>
+
+          {/* Your other components or content can go here */}
+        </div>
+        <div className="TechStackContainer">
+          <div className="TechStackText">
+            <h1>Tech Stack</h1>
+          </div>
+        </div>
+      </>
+    </GeistProvider>
   );
 }
 
---
+++ myapp/src/components/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../App.css";
+const NavbarComponent = () => {
+  return <nav className="navbar navbar-expand-lg "></nav>;
+};
+
+export default NavbarComponent;
